b2064bc053eae81a268d5327336cc36695431616
papafalafel <jeffrodia@gmail.com>
2016-11-13 23:09:38 -0500

    working

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -12,11 +12,19 @@ post '/people' do
   if params[:birthdate].include?("-")
     birthdate = params[:birthdate]
   else
-    birthdate = Date.strptime(params[:birthdate], "%m%d%Y")
-  end
+    birthdate = Date.strptime(params[:birthdate], "%m%d%Y") if !params[:birthdate].empty?
+    end
 
-  person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
-  redirect "/people/#{person.id}"
+  @person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
+    if @person.valid?
+      @person.save
+  redirect "/people/#{@person.id}"
+    else
+    @person.errors.full_messages.each do |msg|
+      @errors = "#{@errors} #{msg}."
+    end
+      erb :"/people/new"
+  end
 end
 
 get '/people/:id/edit' do
@@ -25,12 +33,19 @@ get '/people/:id/edit' do
 end
 
 put '/people/:id' do
-  person = Person.find(params[:id])
-  person.first_name = params[:first_name]
-  person.last_name = params[:last_name]
-  person.birthdate = params[:birthdate]
-  person.save
-  redirect "/people/#{person.id}"
+  @person = Person.find(params[:id])
+  @person.first_name = params[:first_name]
+  @person.last_name = params[:last_name]
+  @person.birthdate = params[:birthdate]
+  if @person.valid?
+    @person.save
+    redirect "/people/#{@person.id}"
+  else
+    @person.errors.full_messages.each do |msg|
+      @errors = "#{@errors} #{msg}."
+    end
+    erb :"/people/edit"
+  end
 end
 
 delete '/people/:id' do
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,7 @@
 class Person < ActiveRecord::Base
 
+validates_presence_of :first_name, :last_name, :birthdate
+
 def self.get_birth_path_num(birthdate)
   # Convert each individual number to an integer and add them together.
   number = birthdate[0].to_i + birthdate[1].to_i + birthdate[2].to_i + birthdate[3].to_i + birthdate[4].to_i + birthdate[5].to_i + birthdate[6].to_i + birthdate[7].to_i
